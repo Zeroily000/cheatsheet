ccf86bc9bc506e918e4aa5fc8b681c2c18f44004
Zeroily000 <zhuzhongjian321@gmail.com>
2024-09-20 16:50:15 -0700

    Give write access to the container

--- toolchain/docker/start_container.sh
+++ toolchain/docker/start_container.sh
@@ -2,10 +2,10 @@
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-docker run -it\
-	--gpus all \
-	-e DISPLAY=$DISPLAY \
-	-v /tmp/.X11-unix/:/tmp/.X11-unix/ \
-	-v $DIR/../../:/workspace/ \
-	--name boiled \
-	dumpling:cuttle
+docker run -it \
+    --gpus all \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    -v $DIR/../../:/workspace/:rw \
+    --name boiled \
+    dumpling:cuttle
