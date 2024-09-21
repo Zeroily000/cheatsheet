4a909030250b6677471c7aef0044956a7c22d0ae
Zeroily000 <zhuzhongjian321@gmail.com>
2024-09-20 17:11:24 -0700

    Simplify boost multi index container example

---
+++ examples/boost_multi_index_container/basic.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "boost/multi_index_container.hpp"
+#include "boost/multi_index/hashed_index.hpp"
+#include "boost/multi_index/indexed_by.hpp"
+#include "boost/multi_index/member.hpp"
+#include "boost/multi_index/mem_fun.hpp"
+#include "boost/multi_index/ordered_index.hpp"
+
+class Foo {
+   public:
+    Foo(std::string name, int id) : name{std::move(name)}, id(id) {}
+
+    int GetId() const {
+        return id;
+    }
+
+   public:
+    std::string name;
+
+   private:
+    int id;
+};
+
+int main() {
+    boost::multi_index_container<
+        std::shared_ptr<Foo>,
+        boost::multi_index::indexed_by<
+            boost::multi_index::hashed_non_unique<boost::multi_index::member<Foo, std::string, &Foo::name>>,
+            boost::multi_index::ordered_unique<boost::multi_index::const_mem_fun<Foo, int, &Foo::GetId>>
+        >
+    > foos;
+
+    foos.insert(std::make_shared<Foo>("A", 1));
+    foos.insert(std::make_shared<Foo>("B", 2));
+    foos.insert(std::make_shared<Foo>("A", 3));
+
+    const auto& index0{foos.get<0>()};
+    const auto range{index0.equal_range("A")};
+    for (auto itr{range.first}; itr != range.second; ++itr) {
+        std::cout << (*itr)->GetId() << std::endl;
+    }
+
+    const auto& index1{foos.get<1>()};
+    const auto itr{index1.find(2)};
+    std::cout << (*itr)->name << std::endl;
+
+    return 0;
+}
+
