655e862dc96dad8296381f6cae5a70bd75fb477e
Zeroily000 <zhuzhongjian321@gmail.com>
2024-09-23 17:29:42 -0700

    More formatting

--- examples/boost_multi_index_container/main.cpp
+++ examples/boost_multi_index_container/main.cpp
@@ -25,25 +25,24 @@ class Foo {
 int main() {
   boost::multi_index_container<
       std::shared_ptr<Foo>,
-      boost::multi_index::indexed_by<
-          boost::multi_index::hashed_non_unique<
-              boost::multi_index::member<Foo, std::string, &Foo::name>>,
-          boost::multi_index::ordered_unique<
-              boost::multi_index::const_mem_fun<Foo, int, &Foo::GetId>>>>
+      boost::multi_index::indexed_by<boost::multi_index::hashed_non_unique<
+                                         boost::multi_index::member<Foo, std::string, &Foo::name>>,
+                                     boost::multi_index::ordered_unique<
+                                         boost::multi_index::const_mem_fun<Foo, int, &Foo::GetId>>>>
       foos;
 
   foos.insert(std::make_shared<Foo>("A", 1));
   foos.insert(std::make_shared<Foo>("B", 2));
   foos.insert(std::make_shared<Foo>("A", 3));
 
-  const auto& index0{foos.get<0>()};
-  const auto range{index0.equal_range("A")};
+  auto const & index0{foos.get<0>()};
+  auto const range{index0.equal_range("A")};
   for (auto itr{range.first}; itr != range.second; ++itr) {
     std::cout << (*itr)->GetId() << std::endl;
   }
 
-  const auto& index1{foos.get<1>()};
-  const auto itr{index1.find(2)};
+  auto const & index1{foos.get<1>()};
+  auto const itr{index1.find(2)};
   std::cout << (*itr)->name << std::endl;
 
   return 0;
