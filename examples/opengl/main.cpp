f7196f0560cfdba3094a8230fef57015581f79a1
Zeroily000 <zhuzhongjian321@gmail.com>
2025-06-12 23:09:24 -0700

    Draw a rectangle and fix gl errors

--- examples/opengl/main.cpp
+++ examples/opengl/main.cpp
@@ -34,9 +34,15 @@ char const * const kFragmentShaderSource{
 };
 
 constexpr float kVertices[]{
-    -0.5f, -0.5f, 0.0f,  // left
-    0.5f,  -0.5f, 0.0f,  // right
-    0.0f,  0.5f,  0.0f   // top
+    0.5f,  0.5f,  0.0f,  // top right
+    0.5f,  -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f, 0.5f,  0.0f   // top left
+};
+
+constexpr unsigned int kIndices[] = {
+    0, 1, 3,  // first Triangle
+    1, 2, 3   // second Triangle
 };
 
 /**
@@ -137,17 +143,22 @@ int main() {
   glGenVertexArrays(1, &vao);
   unsigned int vbo;
   glGenBuffers(1, &vbo);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure
+  unsigned int ebo;
+  glGenBuffers(1, &ebo);
+  // Bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure
   // vertex attributes(s).
   glBindVertexArray(vao);
 
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(kVertices), kVertices, GL_STATIC_DRAW);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(kIndices), kIndices, GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
-  // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex
+  // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex
   // attribute's bound vertex buffer object so afterwards we can safely unbind
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -155,6 +166,9 @@ int main() {
   // this rarely happens. Modifying other VAOs requires a call to glBindVertexArray anyways so we
   // generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
   glBindVertexArray(0);
+
+  // Draw in wireframe polygons.
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   // Render loop
   while (!glfwWindowShouldClose(window)) {
@@ -169,7 +183,8 @@ int main() {
     glUseProgram(shader_program);
     glBindVertexArray(vao);  // seeing as we only have a single VAO there's no need to bind it every
                              // time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0); // no need to unbind it every time
 
     // Swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -181,6 +196,7 @@ int main() {
   // ------------------------------------------------------------------------
   glDeleteVertexArrays(1, &vao);
   glDeleteBuffers(1, &vbo);
+  glDeleteBuffers(1, &ebo);
   glDeleteProgram(shader_program);
 
   // Terminate, clearing all previously allocated GLFW resources.
--- scripts/docker/start_container.sh
+++ scripts/docker/start_container.sh
@@ -4,9 +4,12 @@ xhost +
 
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 docker run -it \
+    --runtime=nvidia \
     --gpus all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
+    -v /dev/dri:/dev/dri \
     -v $DIR/../../:/workspace/:rw \
     --name general \
     general:0.1.0
