bfaa69f5493a05e5af3159bebaaa024099a281c6
Zeroily000 <zhuzhongjian321@gmail.com>
2025-10-26 21:47:59 -0700

    clean up

--- examples/ceres_solver/factors/between_factor.cpp
+++ examples/ceres_solver/factors/between_factor.cpp
@@ -4,11 +4,11 @@
 #include <utility>
 
 BetweenFactor::BetweenFactor(RotationManifold const * const rotation_manifold,
-                             Eigen::Quaterniond a_q_b, Eigen::Vector3d a_t_b,
+                             Eigen::Quaterniond a_q_b, Eigen::Vector3d a_t_ab,
                              Eigen::Matrix<double, kResidualSize, kResidualSize> sqrt_info)
     : rotation_manifold_{rotation_manifold},
       a_q_b_{std::move(a_q_b)},
-      a_t_b_{std::move(a_t_b)},
+      a_t_ab_{std::move(a_t_ab)},
       sqrt_info_{std::move(sqrt_info)} {}
 
 BetweenFactor::~BetweenFactor() = default;
@@ -23,12 +23,12 @@ bool BetweenFactor::Evaluate(double const * const * const parameters, double * c
 }
 
 bool BetweenFactor::RightOplusResidual(double const * const * const parameters,
-                                       double * const residuals, double ** jacobians) const {
+                                       double * const residuals, double ** const jacobians) const {
   Eigen::Map<Eigen::Quaterniond const> const r_q_a{parameters[0]};
-  Eigen::Map<Eigen::Vector3d const> const r_t_a{parameters[1]};
+  Eigen::Map<Eigen::Vector3d const> const r_t_ra{parameters[1]};
 
   Eigen::Map<Eigen::Quaterniond const> const r_q_b{parameters[2]};
-  Eigen::Map<Eigen::Vector3d const> const r_t_b{parameters[3]};
+  Eigen::Map<Eigen::Vector3d const> const r_t_rb{parameters[3]};
 
   // clang-format off
   /**
@@ -37,21 +37,21 @@ bool BetweenFactor::RightOplusResidual(double const * const * const parameters,
    *
    * e = r_T_b^{-1}·r_T_a·z
    *
-   *   = [r_R_b^{-1}, -r_R_b^{-1}·r_t_b]·[r_R_a, r_t_a]·[a_R_b, a_t_b]
-   *     [         0,                 1] [   0,      1] [    0,     1]
+   *   = [r_R_b^{-1}, -r_R_b^{-1}·r_t_rb]·[r_R_a, r_t_ra]·[a_R_b, a_t_ab]
+   *     [         0,                  1] [   0,       1] [    0,      1]
    *
-   *   = [r_R_b^{-1}·r_R_a·a_R_b, r_R_b^{-1}·(r_R_a·a_t_b + r_t_a - r_t_b)]
-   *     [                     0,                                        1]
+   *   = [r_R_b^{-1}·r_R_a·a_R_b, r_R_b^{-1}·r_R_a·a_t_ab + r_R_b^{-1}·(r_t_ra - r_t_rb)]
+   *     [                     0,                                                      1]
    *
    * e_w = Log(r_R_b^{-1}·r_R_a·a_R_b)
-   * e_t = r_R_b^{-1}·(r_R_a·a_t_b + r_t_a - r_t_b)
+   * e_t = r_R_b^{-1}·(r_R_a·a_t_ab + r_t_ra - r_t_rb)
    */
   // clang-format on
   Eigen::Quaterniond const b_q_r{r_q_b.inverse()};
   Eigen::Quaterniond const b_q_a{b_q_r * r_q_a};
   Eigen::Matrix<double, kResidualSize, 1> error;
   error.head<3>() = Sophus::SO3d{b_q_a * a_q_b_}.log();
-  error.tail<3>() = b_q_a * a_t_b_ + b_q_r * (r_t_a - r_t_b);
+  error.tail<3>() = b_q_a * a_t_ab_ + b_q_r * (r_t_ra - r_t_rb);
 
   Eigen::Map<Eigen::Matrix<double, kResidualSize, 1>> res{residuals};
   res = sqrt_info_ * error;
@@ -67,11 +67,11 @@ bool BetweenFactor::RightOplusResidual(double const * const * const parameters,
        *           = d/dwa Log(Exp(e_w + Jr^{-1}(e_w)·(a_R_b^{-1}·wa)))
        *           = Jr^{-1}(e_w)·a_R_b^{-1}
        *
-       * d/dwa e_t = d/dwa (r_R_b^{-1}·r_R_a·Exp(wa)·a_t_b
-       *           = d/dwa (r_R_b^{-1}·r_R_a·(I + [wa]x)·a_t_b
-       *           = d/dwa (r_R_b^{-1}·r_R_a·[wa]x·a_t_b)
-       *           = d/dwa -(r_R_b^{-1}·r_R_a·[a_t_b]x·wa)
-       *           = -r_R_b^{-1}·r_R_a·[a_t_b]x
+       * d/dwa e_t = d/dwa (r_R_b^{-1}·r_R_a·Exp(wa)·a_t_ab
+       *           = d/dwa (r_R_b^{-1}·r_R_a·(I + [wa]x)·a_t_ab
+       *           = d/dwa (r_R_b^{-1}·r_R_a·[wa]x·a_t_ab)
+       *           = d/dwa -(r_R_b^{-1}·r_R_a·[a_t_ab]x·wa)
+       *           = -r_R_b^{-1}·r_R_a·[a_t_ab]x
        */
       // clang-format on
 
@@ -79,7 +79,7 @@ bool BetweenFactor::RightOplusResidual(double const * const * const parameters,
       de_dwa.setZero();
       de_dwa.block<3, 3>(0, 0) =
           Sophus::SO3d::leftJacobianInverse(error.head<3>()) * a_q_b_.inverse().toRotationMatrix();
-      de_dwa.block<3, 3>(3, 0) = b_q_a * Sophus::SO3d::hat(-a_t_b_);
+      de_dwa.block<3, 3>(3, 0) = b_q_a * Sophus::SO3d::hat(-a_t_ab_);
 
       Eigen::Map<Eigen::Matrix<double, kResidualSize, 4, Eigen::RowMajor>> dr_dwa{jacobians[0]};
       dr_dwa = sqrt_info_ * de_dwa;
@@ -108,8 +108,8 @@ bool BetweenFactor::RightOplusResidual(double const * const * const parameters,
        *           = d/dwb Log(Exp(e_w - Jl^{-1}(e_w)·wb))
        *           = -Jl^{-1}(e_w)
        *
-       * d/dwb e_t = d/dwb (r_R_b·Exp(wb))^{-1}·(r_R_a·a_t_b + ref_t_a - ref_t_b)
-       *           = d/dwb Exp(-wb)·r_R_b ^{-1}·(r_R_a·a_t_b + ref_t_a - ref_t_b)
+       * d/dwb e_t = d/dwb (r_R_b·Exp(wb))^{-1}·(r_R_a·a_t_ab + r_t_ra - r_t_rb)
+       *           = d/dwb Exp(-wb)·r_R_b ^{-1}·(r_R_a·a_t_ab + r_t_ra - r_t_rb)
        *           = d/dwb (I - [w]x)·e_t
        *           = d/dwb -[w]x·e_t
        *           = [e_t]x
@@ -146,10 +146,10 @@ bool BetweenFactor::RightOplusResidual(double const * const * const parameters,
 bool BetweenFactor::LeftOplusResidual(double const * const * const parameters,
                                       double * const residuals, double ** jacobians) const {
   Eigen::Map<Eigen::Quaterniond const> const r_q_a{parameters[0]};
-  Eigen::Map<Eigen::Vector3d const> const r_t_a{parameters[1]};
+  Eigen::Map<Eigen::Vector3d const> const r_t_ra{parameters[1]};
 
   Eigen::Map<Eigen::Quaterniond const> const r_q_b{parameters[2]};
-  Eigen::Map<Eigen::Vector3d const> const r_t_b{parameters[3]};
+  Eigen::Map<Eigen::Vector3d const> const r_t_rb{parameters[3]};
 
   // clang-format off
   /**
@@ -158,22 +158,21 @@ bool BetweenFactor::LeftOplusResidual(double const * const * const parameters,
    *
    * e = z·r_T_b^{-1}·r_T_a
    *
-   *   = [a_R_b, a_t_b]·[r_R_b^{-1}, -r_R_b^{-1}·r_t_b]·[r_R_a, r_t_a]
-   *     [    0,     1]   [         0,                   1]   [   0,      1]
+   *   = [a_R_b, a_t_ab]·[r_R_b^{-1}, -r_R_b^{-1}·r_t_rb]·[r_R_a, r_t_ra]
+   *     [    0,      1] [         0,                  1] [   0,      1]
    *
-   *   = [a_R_b·r_R_b^{-1}·r_R_a, a_R_b·r_R_b^{-1}·(r_t_a - r_t_b) + a_t_b]
-   *     [                         0,                                            1]
+   *   = [a_R_b·r_R_b^{-1}·r_R_a, a_R_b·r_R_b^{-1}·(r_t_ra - r_t_rb) + a_t_ab]
+   *     [                     0,                                           1]
    *
    * e_w = Log(a_R_b·r_R_b^{-1}·r_R_a)
-   * e_t = a_R_b·r_R_b^{-1}·(r_t_a - r_t_b) + a_t_b
+   * e_t = a_R_b·r_R_b^{-1}·(r_t_ra - r_t_rb) + a_t_ab
    */
   // clang-format on
   Eigen::Quaterniond const b_q_r{r_q_b.inverse()};
   Eigen::Quaterniond const a_q_r{r_q_a.inverse()};
-  // Eigen::Quaterniond const a_q_r{a_q_b_ * b_q_r};
   Eigen::Matrix<double, kResidualSize, 1> error;
-  error.head<3>() = Sophus::SO3d{a_q_r * r_q_a}.log();
-  error.tail<3>() = a_q_r * (r_t_a - r_t_b) + a_t_b_;
+  error.head<3>() = Sophus::SO3d{a_q_b_ * b_q_r * r_q_a}.log();
+  error.tail<3>() = a_q_b_ * b_q_r * (r_t_ra - r_t_rb) + a_t_ab_;
 
   Eigen::Map<Eigen::Matrix<double, kResidualSize, 1>> res{residuals};
   res = sqrt_info_ * error;
@@ -210,7 +209,7 @@ bool BetweenFactor::LeftOplusResidual(double const * const * const parameters,
       // clang-format on
       Eigen::Matrix<double, kResidualSize, 3, Eigen::RowMajor> de_dta;
       de_dta.setZero();
-      de_dta.block<3, 3>(3, 0) = (a_q_b_*r_q_b.inverse()).toRotationMatrix();
+      de_dta.block<3, 3>(3, 0) = (a_q_b_ * r_q_b.inverse()).toRotationMatrix();
 
       Eigen::Map<Eigen::Matrix<double, kResidualSize, 3, Eigen::RowMajor>> dr_dta{jacobians[1]};
       dr_dta = sqrt_info_ * de_dta;
@@ -225,12 +224,12 @@ bool BetweenFactor::LeftOplusResidual(double const * const * const parameters,
        *           = d/dwb Log(Exp(e_w - Jr^{-1}(e_w)·r_R_a^{-1}·wb))
        *           = -Jr^{-1}(e_w)·r_R_a^{-1}
        *
-       * d/dwb e_t = d/dwb a_R_b·(Exp(wb)·r_R_b)^{-1}·(r_t_a - r_t_b)
-       *           = d/dwb a_R_b·r_R_b^{-1}·Exp(-wb)·(r_t_a - r_t_b)
-       *           = d/dwb a_R_b·r_R_b^{-1}·(I - [wb]x)·(r_t_a - r_t_b)
-       *           = d/dwb a_R_b·r_R_b^{-1}·[-wb]x·(r_t_a - r_t_b)
-       *           = d/dwb a_R_b·r_R_b^{-1}·[r_t_a - r_t_b]x·wb
-       *           = a_R_b·r_R_b^{-1}·[r_t_a - r_t_b]x
+       * d/dwb e_t = d/dwb a_R_b·(Exp(wb)·r_R_b)^{-1}·(r_t_ra - r_t_rb)
+       *           = d/dwb a_R_b·r_R_b^{-1}·Exp(-wb)·(r_t_ra - r_t_rb)
+       *           = d/dwb a_R_b·r_R_b^{-1}·(I - [wb]x)·(r_t_ra - r_t_rb)
+       *           = d/dwb a_R_b·r_R_b^{-1}·[-wb]x·(r_t_ra - r_t_rb)
+       *           = d/dwb a_R_b·r_R_b^{-1}·[r_t_ra - r_t_rb]x·wb
+       *           = a_R_b·r_R_b^{-1}·[r_t_ra - r_t_rb]x
        */
       // clang-format on
       Eigen::Matrix<double, kResidualSize, 4, Eigen::RowMajor> de_dwb;
@@ -238,7 +237,7 @@ bool BetweenFactor::LeftOplusResidual(double const * const * const parameters,
       de_dwb.block<3, 3>(0, 0) =
           -Sophus::SO3d::leftJacobianInverse(-error.head<3>()) * a_q_r.toRotationMatrix();
       de_dwb.block<3, 3>(3, 0) =
-          (a_q_b_ * r_q_b.inverse()).toRotationMatrix() * Sophus::SO3d::hat(r_t_a - r_t_b);
+          (a_q_b_ * r_q_b.inverse()).toRotationMatrix() * Sophus::SO3d::hat(r_t_ra - r_t_rb);
 
       Eigen::Map<Eigen::Matrix<double, kResidualSize, 4, Eigen::RowMajor>> dr_dwb{jacobians[2]};
       dr_dwb = sqrt_info_ * de_dwb;
--- examples/ceres_solver/factors/between_factor.hpp
+++ examples/ceres_solver/factors/between_factor.hpp
@@ -15,7 +15,7 @@ class BetweenFactor : public ceres::SizedCostFunction<6, 4, 3, 4, 3> {
   static std::size_t constexpr kResidualSize{kResidualRotationSize + kResidualTranslationSize};
 
   BetweenFactor(RotationManifold const * rotation_manifold, Eigen::Quaterniond a_q_b,
-                Eigen::Vector3d a_t_b,
+                Eigen::Vector3d a_t_ab,
                 Eigen::Matrix<double, kResidualSize, kResidualSize> sqrt_info);
   ~BetweenFactor() override;
 
@@ -30,7 +30,11 @@ class BetweenFactor : public ceres::SizedCostFunction<6, 4, 3, 4, 3> {
                          double ** jacobians) const;
 
   RotationManifold const * const rotation_manifold_;
+
+  // Rotation from frame b to frame a
   Eigen::Quaterniond a_q_b_;
-  Eigen::Vector3d a_t_b_;
+  // Translation from frame a to frame b, represented in a.
+  Eigen::Vector3d a_t_ab_;
+
   Eigen::Matrix<double, kResidualSize, kResidualSize> sqrt_info_;
 };
